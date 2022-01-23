6f99a1efa1651887318708468878cf53527ac7cd
Abhishek Lohani <abhishek.lohani@macrometa.com>
2022-01-23 19:41:19 +0530

    updated with macrometa icon

--- webapp/app/routes/index.tsx
+++ webapp/app/routes/index.tsx
@@ -6,7 +6,11 @@ export default function Index() {
     <div className="hero">
       <div className="text-center hero-content">
         <div className="max-w-md">
-          <p className="mb-5 text-3xl font-bold">User Management Portal</p>
+          <div className="avatar">
+            <div className="mt-8 rounded-full w-14 h-14">
+              <img src="macrometa-icon.png" alt="macrometa" />
+            </div>
+          </div>
           <p className="mb-5">Built with love by Macrometa</p>
           <Link to={AppPaths.Login}>
             <button className="btn btn-primary">Login</button>
