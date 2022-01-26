60f441b25839420c726fe4b2ec0eb346f4bbcff3
Abhishek Lohani <abhishek.lohani@macrometa.com>
2022-01-26 16:14:55 +0530

    implemented forget me curl

--- webapp/app/utilities/REST/handlers/delete.ts
+++ webapp/app/utilities/REST/handlers/delete.ts
@@ -1,13 +1,16 @@
 import { Fabrics, Queries } from "~/constants";
-import { isPrivateRegion } from "~/utilities/utils";
+import { isMMToken, isPrivateRegion } from "~/utilities/utils";
 import { c8ql } from "../mm";
 import { piiDeleteUser } from "../pii";
 
-export default async (request: Request, form: FormData) => {
+export default async (
+  request: Request,
+  form: FormData,
+  isApiKey: boolean = false
+) => {
   const token = form.get("token")?.toString() ?? "";
-  const country = form.get("country")?.toString() ?? "";
 
-  const isPrivate = isPrivateRegion(country);
+  const isPrivate = !isMMToken(token);
   try {
     if (isPrivate) {
       const resText = await piiDeleteUser(token).then((response) =>
@@ -16,13 +19,32 @@ export default async (request: Request, form: FormData) => {
       // error if expected format is not received
       JSON.parse(resText);
     } else {
-      await c8ql(request, Fabrics.Global, Queries.DeleteUser(), {
-        token,
-      });
+      const userRes = await c8ql(
+        request,
+        Fabrics.Global,
+        Queries.DeleteUser(),
+        {
+          token,
+        },
+        isApiKey
+      ).then((request) => request.json());
+      if (userRes?.error) {
+        throw new Error(JSON.stringify(userRes));
+      }
     }
-    await c8ql(request, Fabrics.Global, Queries.DeleteLocation(), {
-      token,
-    });
+
+    const locationRes = await c8ql(
+      request,
+      Fabrics.Global,
+      Queries.DeleteLocation(),
+      {
+        token,
+      },
+      isApiKey
+    ).then((request) => request.json());
+    if (locationRes?.error) {
+      throw new Error(JSON.stringify(locationRes));
+    }
     return { isPrivate };
   } catch (error: any) {
     return { error: true, errorMessage: error?.message, name: error?.name };
